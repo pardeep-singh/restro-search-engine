3f8965b1904a9b6a2208d66742566033d39fe765
Pardeep Singh <pardeep@helpshift.com>
2018-01-19 14:17:28 +0530

    Update project.clj file

--- project.clj
+++ project.clj
@@ -1,12 +1,16 @@
 (defproject restro-search-engine "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Restaurants Search Engine powered by Elasticsearch"
   :url "http://example.com/FIXME"
+  :license {:name "Proprietary"}
+  :uberjar-exclusions [#"(?i)^META-INF/[^/]*\.(SF|RSA)$"]
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]]
-  :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler restro-search-engine.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+  :source-paths ["src"]
+  :global-vars {*warn-on-reflection* true}
+  :manifest {"Project-Name" ~#(:name %)
+             "Project-Version" ~#(:version %)
+             "Build-Date" ~(str (java.util.Date.))}
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.0"]]}})
